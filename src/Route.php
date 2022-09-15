4081f0307622de97a6227987e9a1b29b2200b35d
Juan Felipe Rivera G <juan.rivera@alegra.com>
2022-09-15 12:32:05 -0500

    Change router behavior

---
+++ src/Route.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Awesome;
+
+/**
+ * Route class
+ * @package Awesome
+ */
+class Route
+{
+    /**
+     * Route path
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Regex path
+     * @var string
+     */
+    protected $regexPath;
+
+    /**
+     * Route method
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * Route callback
+     * @var callable
+     */
+    protected $callback;
+
+    /**
+     * Route params
+     * @var array
+     */
+    protected $params;
+
+    /**
+     * Route constructor
+     * @param mixed $args
+     */
+    public function __construct($args)
+    {
+        extract($args);
+
+        try {
+            $this->path = $path;
+            $this->method = $method;
+            $this->callback = $callback;
+            $this->regexPath = $regexPath;
+            $this->params = $params;
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Get route path
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Get route method
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * Get route callback
+     * @return callable
+     */
+    public function getCallback()
+    {
+        return $this->callback;
+    }
+
+    /**
+     * Get route regex path
+     * @return string
+     */
+    public function getRegexPath()
+    {
+        return $this->regexPath;
+    }
+
+    /**
+     * Set route regex path
+     * @param string $regexPath
+     * @return void
+     */
+    public function setRegexPath(string $regexPath)
+    {
+        $this->regexPath = $regexPath;
+    }
+
+    /**
+     * Get route params
+     * @return array
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    /**
+     * Set route params
+     * @param array $params
+     * @return void
+     */
+    public function setParams(array $params)
+    {
+        $this->params = $params;
+    }
+
+    /**
+     * Has callable
+     * @return bool
+     */
+    public function hasCallable()
+    {
+        return is_callable($this->callback);
+    }
+
+    /**
+     * Call route callback
+     * @return mixed
+     */
+    public function call()
+    {
+        if ($this->hasCallable()) {
+            call_user_func_array($this->callback, []);
+        }
+    }
+}
