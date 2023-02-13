ebd31c13dfb5238f3e9dd828a144cff06dd3fdf6
Juan Felipe Rivera G <juan.rivera@alegra.com>
2023-02-12 22:50:55 -0500

    Fix syntax error

--- src/Controller.php
+++ src/Controller.php
@@ -97,7 +97,7 @@ abstract class Controller
 
         if (!App::isCli()) {
             echo $finalResponse;
-            return;
+            return null;
         }
 
         return $finalResponse;
